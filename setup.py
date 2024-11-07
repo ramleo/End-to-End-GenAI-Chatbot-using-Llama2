21abfebf4ba93622a2cd9273de99d4b49c8d3e87
ramleo <66366261+ramleo@users.noreply.github.com>
2024-11-07 15:43:00 +0530

    Add setup.py and run requirements.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenAI Chatbot',
+    version= '0.0.1',
+    author= 'W Ram',
+    author_email= 'ramleo84@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
